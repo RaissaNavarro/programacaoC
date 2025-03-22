ff6a0754043a5ec54b75ca1a3d5f39a02b1366b6
Raissa Navarro <144500102+RaissaNavarro@users.noreply.github.com>
2025-03-22 18:07:05 +0000

    criação de outra pasta

---
+++ ni1_nc4.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+ //nn pode começar nenhuma variavel com número.
+ // eu posso criar uma variavel com a letra minuscula e a outra com maiuscula.
+int main() {
+    int idade;
+    int quantidade;
+    float altura;
+    double peso;
+    char letra;
+    char nome[];
+    int _teste;
+
+
+}
