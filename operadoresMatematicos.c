9c8b761bc95188aafd7af03eb3d24deaf9e1047e
Raissa <Raissanavarro66@gmail.com>
2025-03-24 19:34:02 -0300

    adicionando variaveis

---
+++ operadoresMatematicos.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(){
+    int numero1, numero2;
+    int soma, subtracao, multiplicacao, divisao;
+
+    printf("Entre com o número 1: \n");
+    scanf("%d", numero1);
+    printf("Entre com o número 2: \n");
+    scanf("%d", &subtracao);
+
+    soma = numero1 + numero2; //soma
+
+    subtracao = numero1 - numero2; // subtração
+
+    multiplicacao = numero1 * numero2; // multiplicação
+
+    divisao = numero1 / numero2; // divisão
+
+    printf("A soma é: %d \n", soma);
+    printf("A subtração é: %d \n", subtracao);
+    printf("A multiplicação é: %d \n", multiplicacao);
+    printf("A divisão é: %d \n", divisao);
+
+
+
+}
