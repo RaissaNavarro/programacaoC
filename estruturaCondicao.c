d3e27e237ef6c5ef1911d3781a367641c399682e
Raissa Navarro <144500102+RaissaNavarro@users.noreply.github.com>
2025-03-26 00:23:24 +0000

    mexendo com if e else em C

---
+++ estruturaCondicao.c
@@ -0,0 +1,12 @@
+// #include <stdio.h>
+// int main() {
+//     int  numero1, numero2;
+//     numero1 = 10;
+//     numero2 = 5;
+//     if (numero1 > nu
+//         printf("Número 1 é maior que número 2");
+//     }else   {
+//     printf("Número 2 é maior do que o número 1");
+//     }
+//     printf("Fora IF");
+// }
