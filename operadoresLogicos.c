454e8513ffdef8681aa27c934dd531935cbe525c
Raissa Navarro <144500102+RaissaNavarro@users.noreply.github.com>
2025-03-27 23:27:59 +0000

    operadores lógicos

---
+++ operadoresLogicos.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main() {
+//   int a = 5;
+//   int b = 10;
+
+//   if (a > 0 && b > 0) {
+//     printf("Ambos os números são positivos\n");
+//   }
+
+//   return 0;
+
+// int a = 5;
+// int b = -10;
+
+// if (a > 0 || b > 0) {
+//   printf("Pelo menos um dos números é positivo\n");
+// }
+
+// return 0;
+
+// int a = -5;
+
+// if (!(a > 0)) {
+//   printf("a não é um número positivo\n");
+// }
+
+// return 0;
+
+
+
+}
